29aa75497694072c886e710e147e006b10b56f16
AndreHermanto <andre.hermanto93@gmail.com>
2018-04-23 10:17:18 +1000

    Remove disabling submit button (#60)
    * Remove disabling submit button
    
    Closes: https://github.com/GenomeOne/web-seed/issues/59
    
    * Remove valid

--- src/Examples/ExamplesForm.js
+++ src/Examples/ExamplesForm.js
@@ -4,7 +4,7 @@ import { Form } from 'semantic-ui-react';
 import { Heading, Fields, Buttons } from 'web-components';
 import { reduxForm } from 'redux-form/immutable';
 
-const ExamplesForm = ({ initialValues, handleSubmit, onCancel, valid }) => (
+const ExamplesForm = ({ initialValues, handleSubmit, onCancel }) => (
   <Form onSubmit={handleSubmit}>
     <Heading size="h1">{initialValues ? 'Update Example' : 'Create Example'}</Heading>
     <Fields.Text name="title" required />
@@ -23,7 +23,6 @@ const ExamplesForm = ({ initialValues, handleSubmit, onCancel, valid }) => (
           {
             content: initialValues ? 'Update Example' : 'Create Example',
             type: 'submit',
-            disabled: !valid,
           },
           {
             content: 'Cancel',
@@ -43,7 +42,6 @@ ExamplesForm.propTypes = {
   }),
   onCancel: PropTypes.func.isRequired,
   handleSubmit: PropTypes.func.isRequired,
-  valid: PropTypes.bool.isRequired,
 };
 ExamplesForm.defaultProps = {
   initialValues: undefined,
