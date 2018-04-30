f2a3eda13df11fc18badce3ec5aa6c5189f82c5c
Craig McNamara <cmcnamara87@gmail.com>
2018-05-01 09:59:19 +1000

    remove normalizr
    Problem: Normalizr is being exported
    from web-components, so we dont need
    it as a dependency anymore.
    
    Solution: Remove normalizr.

--- src/Examples/schemas.js
+++ src/Examples/schemas.js
@@ -1,4 +1,4 @@
-import { schema } from 'normalizr';
+import { schema } from 'web-components';
 
 export const exampleSchema = new schema.Entity('examples');
 export const examplesSchema = [exampleSchema];
