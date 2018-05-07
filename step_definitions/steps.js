7bfa470ae2bd70d247246de0a8fc21a50a6156f2
AndreHermanto <andre.hermanto93@gmail.com>
2018-05-07 19:33:40 +1000

    Fix e2e syntax

--- step_definitions/steps.js
+++ step_definitions/steps.js
@@ -10,34 +10,26 @@ const defaultWaitTime = 5000;
 
 Given(/^I (?:browse|open|visit).*? `(.*?)`$/, pageName => client.url(pages[pageName]));
 
-Then(/^I (?:wait).*? to (?:find|identify|see|spot).*? (`.*`).*?$/, (selectorChain) => {
-  client.useCss().waitForElementVisible(createNestedSelector(selectorChain), defaultWaitTime);
-});
+Then(/^I (?:wait).*? to (?:find|identify|see|spot).*? (`.*`).*?$/, selectorChain => client
+  .useCss()
+  .waitForElementVisible(createNestedSelector(selectorChain), defaultWaitTime));
 
-Then(/^I (?:find|identify|see|spot).*? (`.*`).*?$/, (selectorChain) => {
-  client.useCss().expect.element(createNestedSelector(selectorChain)).to.be.visible; // eslint-disable-line
-});
+Then(/^I (?:find|identify|see|spot).*? (`.*`).*?$/, selectorChain => 
+  client.useCss().expect.element(createNestedSelector(selectorChain)).to.be.visible);
 
-Then(/^I (?:can|don)'t (?:find|identify|see|spot).*? (`.*`).*?$/, (selectorChain) => {
-  client.useCss().expect.element(createNestedSelector(selectorChain)).to.not.be.visible; // eslint-disable-line
-});
+Then(/^I (?:can|don)'t (?:find|identify|see|spot).*? (`.*`).*?$/, selectorChain => 
+  client.useCss().expect.element(createNestedSelector(selectorChain)).to.not.be.visible);
 
-When(/^I (?:enter|input|supply|type).*? "(.*?)" in.*? (`.*`)$/, (value, selectorChain) => {
-  client
-    .useCss()
-    .waitForElementVisible(createNestedSelector(selectorChain), defaultWaitTime)
-    .setValue(createNestedSelector(selectorChain), value);
-});
+When(/^I (?:enter|input|supply|type).*? "(.*?)" in.*? (`.*`)$/, (value, selectorChain) => client
+  .useCss()
+  .waitForElementVisible(createNestedSelector(selectorChain), defaultWaitTime)
+  .setValue(createNestedSelector(selectorChain), value));
 
-When(/^I (?:check).*? (`.*`) not empty$/, (value) => {
-  client.useCss().getText(createNestedSelector(value), (result) => {
-    this.assert.notEqual('', result.value);
-  });
-});
+When(/^I (?:check).*? (`.*`) not empty$/, value => client.useCss().getText(createNestedSelector(value), (result) => {
+  this.assert.notEqual('', result.value);
+}));
 
-When(/^I (?:activate|click).*? (`.*`)$/, (selectorChain) => {
-  client
-    .useCss()
-    .waitForElementVisible(createNestedSelector(selectorChain), defaultWaitTime)
-    .click(createNestedSelector(selectorChain));
-});
+When(/^I (?:activate|click).*? (`.*`)$/, selectorChain => client
+  .useCss()
+  .waitForElementVisible(createNestedSelector(selectorChain), defaultWaitTime)
+  .click(createNestedSelector(selectorChain)));
