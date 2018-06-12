4c4237e99ddbc614218e9b4be0bccbe1bcdc6b31
Nguyen Nguyen <khoi.nguyen@genome.one>
2018-06-12 11:42:17 +1000

    (feature): show, edit, delete glossary terms! Close: #174 Close: #175 Close: #176

---
+++ src/GlossaryTerms/GlossaryTermsEdit.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { QueryResource, Mutation } from 'web-components';
+import { glossaryTermSchema } from './schemas';
+import GlossaryTermsForm from './GlossaryTermsForm';
+/* eslint class-methods-use-this: 0 */
+/* eslint no-case-declarations: 0 */
+export default function GlossaryTermsEdit({
+  closePanel,
+  match: {
+    params: { glossaryTermId },
+  },
+}) {
+  return (
+    <QueryResource
+      queries={[
+        {
+          resourceName: 'glossaryTerms',
+          url: `/glossary-terms/${glossaryTermId}`,
+          schema: glossaryTermSchema,
+        },
+      ]}
+      render={({ glossaryTerms }) => {
+        const glossaryTerm = glossaryTerms[0];
+        return (
+          <Mutation
+            resourceName="glossaryTerms"
+            url={`/glossary-terms/${glossaryTermId}`}
+            schema={glossaryTermSchema}
+            post={closePanel}
+            render={({ update, loading: updateLoading }) => {
+              if (updateLoading) {
+                return <div>loading...</div>;
+              }
+
+              return (
+                <GlossaryTermsForm
+                  onSubmit={update}
+                  onCancel={closePanel}
+                  initialValues={glossaryTerm}
+                />
+              );
+            }}
+          />
+        );
+      }}
+    />
+  );
+}
+
+GlossaryTermsEdit.propTypes = {
+  closePanel: PropTypes.func.isRequired,
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      glossaryTermId: PropTypes.string.isRequired,
+    }).isRequired,
+  }).isRequired,
+};
