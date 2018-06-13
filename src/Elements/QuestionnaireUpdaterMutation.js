1cfba449d86e6d7d4fc95f4614f1ad25b7287821
Li Xu <li.xu@genome.one>
2018-06-13 17:24:18 +1000

    added a check to update missing Id in answers

--- src/Elements/QuestionnaireUpdaterMutation.js
+++ src/Elements/QuestionnaireUpdaterMutation.js
@@ -5,6 +5,19 @@ import { Iterable } from 'immutable';
 import { Mutation } from 'web-components';
 import { versionSchema, questionnaireSchema } from '../Questionnaires/schemas';
 
+const updateAnswersId = ({ body, ...rest }) => {
+  const updatedBody = body.map(({ answers, ...questionRest }) => {
+    const updatedAnswersWithId = answers.map((answer) => {
+      if (!answer.id) {
+        return { ...answer, id: cuid() };
+      }
+      return answer;
+    });
+    return { ...questionRest, answers: updatedAnswersWithId };
+  });
+  return { body: updatedBody, ...rest };
+};
+
 export default function QuestionnaireUpdaterMutation({ post, render, version, questionnaire }) {
   return (
     <Mutation
@@ -39,7 +52,7 @@ export default function QuestionnaireUpdaterMutation({ post, render, version, qu
                   ...version.body.slice(insertAtIndex),
                 ],
               };
-              return create(newVersion);
+              return create(updateAnswersId(newVersion));
             };
             const updateQuestionnaire = (payload) => {
               // find the question with the same id in the version
@@ -56,7 +69,7 @@ export default function QuestionnaireUpdaterMutation({ post, render, version, qu
                   return element;
                 }),
               };
-              return create(newVersion);
+              return create(updateAnswersId(newVersion));
             };
             const removeElement = (elementId) => {
               // find the question with the same id in the version
@@ -67,7 +80,7 @@ export default function QuestionnaireUpdaterMutation({ post, render, version, qu
                 ...version,
                 body: version.body.filter(element => element.id !== elementId),
               };
-              return create(newVersion);
+              return create(updateAnswersId(newVersion));
             };
             const moveElement = (elementId, moveToIndex) => {
               // find current element index
@@ -78,7 +91,7 @@ export default function QuestionnaireUpdaterMutation({ post, render, version, qu
                 ...version,
                 body: version.body,
               };
-              return create(newVersion);
+              return create(updateAnswersId(newVersion));
             };
             return render({
               update: updateQuestionnaire,
