3b172669755a9c7b1ccc0039b6c30707cf97c52c
Nguyen Nguyen <khoi.nguyen@genome.one>
2018-06-29 14:23:05 +1000

    (feature): display trait error message! Close: #305

--- src/Elements/ElementsAddTraitForm.js
+++ src/Elements/ElementsAddTraitForm.js
@@ -1,29 +1,49 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Form } from 'semantic-ui-react';
+import { Form, Message } from 'semantic-ui-react';
 import { Heading, Fields, Buttons } from 'web-components';
 import { reduxForm } from 'redux-form/immutable';
 
-const ElementsAddTraitForm = ({ handleSubmit, onCancel, traits }) => (
-  <Form onSubmit={handleSubmit}>
-    <Heading size="h1">Element Trait</Heading>
-    <Fields.Select label="Trait" name="trait" options={traits} required />
-
-    <Buttons
-      actions={[
-        {
-          content: 'Save',
-          type: 'submit',
-        },
-        {
-          content: 'Cancel',
-          onClick: onCancel,
-          type: 'button',
-        },
-      ]}
-    />
-  </Form>
-);
+const ElementsAddTraitForm = ({ handleSubmit, onCancel, traits, submitting }) => {
+  if (!traits || traits.length === 0) {
+    return (
+      <div>
+        <Message negative>
+          <p>Sorry, this question type cant have traits associated with it.</p>
+        </Message>
+        <Buttons
+          actions={[
+            {
+              content: 'Cancel',
+              onClick: onCancel,
+              type: 'button',
+            },
+          ]}
+        />
+      </div>
+    );
+  }
+  return (
+    <Form onSubmit={handleSubmit}>
+      <Heading size="h1">Element Trait</Heading>
+      <Fields.Select label="Trait" name="trait" options={traits} required />
+      <Buttons
+        actions={[
+          {
+            content: 'Save',
+            type: 'submit',
+            disabled: submitting,
+          },
+          {
+            content: 'Cancel',
+            onClick: onCancel,
+            type: 'button',
+          },
+        ]}
+      />
+    </Form>
+  );
+};
 ElementsAddTraitForm.propTypes = {
   onCancel: PropTypes.func.isRequired,
   handleSubmit: PropTypes.func.isRequired,
@@ -32,6 +52,7 @@ ElementsAddTraitForm.propTypes = {
     value: PropTypes.string.isRequired,
     text: PropTypes.string.isRequired,
   }).isRequired,
+  submitting: PropTypes.bool.isRequired,
 };
 
 export default reduxForm({
